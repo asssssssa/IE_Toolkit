c3388d48c899deb83e28e0f46def0d4b87c1f06d
assssssssssa <wugeyuequ@163.com>
2023-10-10 08:55:37 +0800

    add ie toll

---
+++ ie_tool/ie_tool_trainer.py
@@ -0,0 +1,281 @@
+import argparse
+import math
+import os
+from typing import Type
+
+import torch
+from torch.nn import DataParallel
+from torch.optim import Optimizer
+import transformers
+from torch.utils.data import DataLoader
+from transformers import AdamW, BertConfig
+from transformers import BertTokenizer, AutoTokenizer
+
+from ie_tool import models, prediction
+from ie_tool import sampling
+from ie_tool import util
+from ie_tool.entities import Dataset
+from ie_tool.evaluator import Evaluator
+from ie_tool.input_reader import JsonInputReader, BaseInputReader
+from tqdm import tqdm
+from ie_tool.trainer import BaseTrainer
+
+SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+class IEToolTrainer(BaseTrainer):
+    """ Joint entity and relation extraction training and evaluation """
+
+    def __init__(self, args: argparse.Namespace):
+        super().__init__(args)
+
+        # byte-pair encoding
+        print (args.tokenizer_path)
+        self._tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_path,
+                                                        do_lower_case=args.lowercase,
+                                                        cache_dir=args.cache_path)
+
+    def eval(self, dataset_path: str, types_path: str, input_reader_cls: Type[BaseInputReader]):
+        args = self._args
+        dataset_label = 'test'
+
+        self._logger.info("Dataset: %s" % dataset_path)
+        self._logger.info("Model: %s" % args.model_type)
+
+        # create log csv files
+        self._init_eval_logging(dataset_label)
+
+        # read datasets
+        input_reader = input_reader_cls(types_path, self._tokenizer,
+                                        max_span_size=args.max_span_size, logger=self._logger)
+        test_dataset = input_reader.read(dataset_path, dataset_label)
+        self._log_datasets(input_reader)
+
+        # load model
+        model = self._load_model(input_reader)
+        model.to(self._device)
+
+        # evaluate
+        self._eval(model, test_dataset, input_reader)
+
+        self._logger.info("Logged in: %s" % self._log_path)
+        self._close_summary_writer()
+
+    def predict(self, dataset_path: str, types_path: str, input_reader_cls: Type[BaseInputReader]):
+        args = self._args
+
+        # read datasets
+        input_reader = input_reader_cls(types_path, self._tokenizer,
+                                        max_span_size=args.max_span_size,
+                                        spacy_model=args.spacy_model)
+        dataset = input_reader.read(dataset_path, 'dataset')
+
+        model = self._load_model(input_reader)
+        model.to(self._device)
+
+        self._predict(model, dataset, input_reader)
+
+    def _load_model(self, input_reader):
+        model_class = models.get_model(self._args.model_type)
+
+        config = BertConfig.from_pretrained(self._args.model_path, cache_dir=self._args.cache_path)
+        # util.check_version(config, model_class, self._args.model_path)
+
+        model = model_class.from_pretrained(self._args.model_path,
+                                            config=config,
+                                            # SpERT model parameters
+                                            cls_token=self._tokenizer.convert_tokens_to_ids('[CLS]'),
+                                            relation_types=input_reader.relation_type_count - 1,
+                                            entity_types=input_reader.entity_type_count,
+                                            max_pairs=self._args.max_pairs,
+                                            prop_drop=self._args.prop_drop,
+                                            size_embedding=self._args.size_embedding,
+                                            freeze_transformer=self._args.freeze_transformer,
+                                            cache_dir=self._args.cache_path)
+
+
+        return model
+
+    def _eval(self, model: torch.nn.Module, dataset: Dataset, input_reader: BaseInputReader,
+              epoch: int = 0, updates_epoch: int = 0, iteration: int = 0):
+        self._logger.info("Evaluate: %s" % dataset.label)
+
+        if isinstance(model, DataParallel):
+            # currently no multi GPU support during evaluation
+            model = model.module
+
+        # create evaluator
+        predictions_path = os.path.join(self._log_path, f'predictions_{dataset.label}_epoch_{epoch}.json')
+        examples_path = os.path.join(self._log_path, f'examples_%s_{dataset.label}_epoch_{epoch}.html')
+        evaluator = Evaluator(dataset, input_reader, self._tokenizer,
+                              self._args.rel_filter_threshold, self._args.no_overlapping, predictions_path,
+                              examples_path, self._args.example_count)
+
+        # create data loader
+        dataset.switch_mode(Dataset.EVAL_MODE)
+        data_loader = DataLoader(dataset, batch_size=self._args.eval_batch_size, shuffle=False, drop_last=False,
+                                 num_workers=self._args.sampling_processes, collate_fn=sampling.collate_fn_padding)
+
+        with torch.no_grad():
+            model.eval()
+
+            # iterate batches
+            total = math.ceil(dataset.document_count / self._args.eval_batch_size)
+            for batch in tqdm(data_loader, total=total, desc='Evaluate epoch %s' % epoch):
+                # move batch to selected device
+                batch = util.to_device(batch, self._device)
+
+                # run model (forward pass)
+                result = model(encodings=batch['encodings'], context_masks=batch['context_masks'],
+                               entity_masks=batch['entity_masks'], entity_sizes=batch['entity_sizes'],
+                               entity_spans=batch['entity_spans'], entity_sample_masks=batch['entity_sample_masks'],
+                               inference=True)
+                entity_clf, rel_clf, rels = result
+
+                # evaluate batch
+                evaluator.eval_batch(entity_clf, rel_clf, rels, batch)
+
+        global_iteration = epoch * updates_epoch + iteration
+        ner_eval, rel_eval, rel_nec_eval = evaluator.compute_scores()
+        self._log_eval(*ner_eval, *rel_eval, *rel_nec_eval,
+                       epoch, iteration, global_iteration, dataset.label)
+
+        if self._args.store_predictions and not self._args.no_overlapping:
+            evaluator.store_predictions()
+
+        if self._args.store_examples:
+            evaluator.store_examples()
+
+    def _predict(self, model: torch.nn.Module, dataset: Dataset, input_reader: BaseInputReader):
+        # create data loader
+        dataset.switch_mode(Dataset.EVAL_MODE)
+        data_loader = DataLoader(dataset, batch_size=self._args.eval_batch_size, shuffle=False, drop_last=False,
+                                 num_workers=self._args.sampling_processes, collate_fn=sampling.collate_fn_padding)
+
+        pred_entities = []
+        pred_relations = []
+
+        with torch.no_grad():
+            model.eval()
+
+            # iterate batches
+            total = math.ceil(dataset.document_count / self._args.eval_batch_size)
+            for batch in tqdm(data_loader, total=total, desc='Predict'):
+                # move batch to selected device
+                batch = util.to_device(batch, self._device)
+
+                # run model (forward pass)
+                result = model(encodings=batch['encodings'], context_masks=batch['context_masks'],
+                               entity_masks=batch['entity_masks'], entity_sizes=batch['entity_sizes'],
+                               entity_spans=batch['entity_spans'], entity_sample_masks=batch['entity_sample_masks'],
+                               inference=True)
+                entity_clf, rel_clf, rels = result
+
+                # convert predictions
+                predictions = prediction.convert_predictions(entity_clf, rel_clf, rels,
+                                                             batch, self._args.rel_filter_threshold,
+                                                             input_reader)
+
+                batch_pred_entities, batch_pred_relations = predictions
+                pred_entities.extend(batch_pred_entities)
+                pred_relations.extend(batch_pred_relations)
+
+        prediction.store_predictions(dataset.documents, pred_entities, pred_relations, self._args.predictions_path)
+
+    def _get_optimizer_params(self, model):
+        param_optimizer = list(model.named_parameters())
+        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+        optimizer_params = [
+            {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+             'weight_decay': self._args.weight_decay},
+            {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}]
+
+        return optimizer_params
+
+    def _log_train(self, optimizer: Optimizer, loss: float, epoch: int,
+                   iteration: int, global_iteration: int, label: str):
+        # average loss
+        avg_loss = loss / self._args.train_batch_size
+        # get current learning rate
+        lr = self._get_lr(optimizer)[0]
+
+        # log to tensorboard
+        self._log_tensorboard(label, 'loss', loss, global_iteration)
+        self._log_tensorboard(label, 'loss_avg', avg_loss, global_iteration)
+        self._log_tensorboard(label, 'lr', lr, global_iteration)
+
+        # log to csv
+        self._log_csv(label, 'loss', loss, epoch, iteration, global_iteration)
+        self._log_csv(label, 'loss_avg', avg_loss, epoch, iteration, global_iteration)
+        self._log_csv(label, 'lr', lr, epoch, iteration, global_iteration)
+
+    def _log_eval(self, ner_prec_micro: float, ner_rec_micro: float, ner_f1_micro: float,
+                  ner_prec_macro: float, ner_rec_macro: float, ner_f1_macro: float,
+
+                  rel_prec_micro: float, rel_rec_micro: float, rel_f1_micro: float,
+                  rel_prec_macro: float, rel_rec_macro: float, rel_f1_macro: float,
+
+                  rel_nec_prec_micro: float, rel_nec_rec_micro: float, rel_nec_f1_micro: float,
+                  rel_nec_prec_macro: float, rel_nec_rec_macro: float, rel_nec_f1_macro: float,
+                  epoch: int, iteration: int, global_iteration: int, label: str):
+
+        # log to tensorboard
+        self._log_tensorboard(label, 'eval/ner_prec_micro', ner_prec_micro, global_iteration)
+        self._log_tensorboard(label, 'eval/ner_recall_micro', ner_rec_micro, global_iteration)
+        self._log_tensorboard(label, 'eval/ner_f1_micro', ner_f1_micro, global_iteration)
+        self._log_tensorboard(label, 'eval/ner_prec_macro', ner_prec_macro, global_iteration)
+        self._log_tensorboard(label, 'eval/ner_recall_macro', ner_rec_macro, global_iteration)
+        self._log_tensorboard(label, 'eval/ner_f1_macro', ner_f1_macro, global_iteration)
+
+        self._log_tensorboard(label, 'eval/rel_prec_micro', rel_prec_micro, global_iteration)
+        self._log_tensorboard(label, 'eval/rel_recall_micro', rel_rec_micro, global_iteration)
+        self._log_tensorboard(label, 'eval/rel_f1_micro', rel_f1_micro, global_iteration)
+        self._log_tensorboard(label, 'eval/rel_prec_macro', rel_prec_macro, global_iteration)
+        self._log_tensorboard(label, 'eval/rel_recall_macro', rel_rec_macro, global_iteration)
+        self._log_tensorboard(label, 'eval/rel_f1_macro', rel_f1_macro, global_iteration)
+
+        self._log_tensorboard(label, 'eval/rel_nec_prec_micro', rel_nec_prec_micro, global_iteration)
+        self._log_tensorboard(label, 'eval/rel_nec_recall_micro', rel_nec_rec_micro, global_iteration)
+        self._log_tensorboard(label, 'eval/rel_nec_f1_micro', rel_nec_f1_micro, global_iteration)
+        self._log_tensorboard(label, 'eval/rel_nec_prec_macro', rel_nec_prec_macro, global_iteration)
+        self._log_tensorboard(label, 'eval/rel_nec_recall_macro', rel_nec_rec_macro, global_iteration)
+        self._log_tensorboard(label, 'eval/rel_nec_f1_macro', rel_nec_f1_macro, global_iteration)
+
+        # log to csv
+        self._log_csv(label, 'eval', ner_prec_micro, ner_rec_micro, ner_f1_micro,
+                      ner_prec_macro, ner_rec_macro, ner_f1_macro,
+
+                      rel_prec_micro, rel_rec_micro, rel_f1_micro,
+                      rel_prec_macro, rel_rec_macro, rel_f1_macro,
+
+                      rel_nec_prec_micro, rel_nec_rec_micro, rel_nec_f1_micro,
+                      rel_nec_prec_macro, rel_nec_rec_macro, rel_nec_f1_macro,
+                      epoch, iteration, global_iteration)
+
+    def _log_datasets(self, input_reader):
+        self._logger.info("Relation type count: %s" % input_reader.relation_type_count)
+        self._logger.info("Entity type count: %s" % input_reader.entity_type_count)
+
+        self._logger.info("Entities:")
+        for e in input_reader.entity_types.values():
+            self._logger.info(e.verbose_name + '=' + str(e.index))
+
+        self._logger.info("Relations:")
+        for r in input_reader.relation_types.values():
+            self._logger.info(r.verbose_name + '=' + str(r.index))
+
+        for k, d in input_reader.datasets.items():
+            self._logger.info('Dataset: %s' % k)
+            self._logger.info("Document count: %s" % d.document_count)
+            self._logger.info("Relation count: %s" % d.relation_count)
+            self._logger.info("Entity count: %s" % d.entity_count)
+
+    def _init_eval_logging(self, label):
+        self._add_dataset_logging(label,
+                                  data={'eval': ['ner_prec_micro', 'ner_rec_micro', 'ner_f1_micro',
+                                                 'ner_prec_macro', 'ner_rec_macro', 'ner_f1_macro',
+                                                 'rel_prec_micro', 'rel_rec_micro', 'rel_f1_micro',
+                                                 'rel_prec_macro', 'rel_rec_macro', 'rel_f1_macro',
+                                                 'rel_nec_prec_micro', 'rel_nec_rec_micro', 'rel_nec_f1_micro',
+                                                 'rel_nec_prec_macro', 'rel_nec_rec_macro', 'rel_nec_f1_macro',
+                                                 'epoch', 'iteration', 'global_iteration']})
